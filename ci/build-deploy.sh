dd97d0776d7d63fdea901e63affe7587788018ea
gr0vity-dev <iq.cc@pm.me>
2023-06-19 14:51:51 +0200

    Use build.sh script under the hood
    - remove the cmake command
    - Initialise with the correct ENV variables

--- ci/build-deploy.sh
+++ ci/build-deploy.sh
@@ -1,45 +1,27 @@
+new file mode 100755
 #!/bin/bash
 
-qt_dir=${1}
-ci_version_pre_release="OFF"
-if [[ -n "${CI_VERSION_PRE_RELEASE}" ]]; then
-    ci_version_pre_release="$CI_VERSION_PRE_RELEASE"
-fi
+export QT_DIR=${1}
+export NANO_GUI="ON"
+export PORTABLE="1"
+export CI_VERSION_PRE_RELEASE=${CI_VERSION_PRE_RELEASE:-"OFF"}
+export COMPILER=${COMPILER:-"gcc"}
 
 set -o errexit
 set -o nounset
 set -o xtrace
 OS=$(uname)
 
-mkdir build
-pushd build
-CONFIGURATION="Release"
-
 if [[ "${NETWORK}" == "BETA" ]]; then
-    NETWORK_CFG="beta"
-    CONFIGURATION="RelWithDebInfo"
+    export NANO_NETWORK="beta"
+    export BUILD_TYPE="RelWithDebInfo"
 elif [[ "${NETWORK}" == "TEST" ]]; then
-    NETWORK_CFG="test"
+    export NANO_NETWORK="test"
+    export BUILD_TYPE="RelWithDebInfo"
 else
-    NETWORK_CFG="live"
+    export NANO_NETWORK="live"
+    export BUILD_TYPE="Release"
 fi
 
-cmake \
--G'Unix Makefiles' \
--DACTIVE_NETWORK=nano_${NETWORK_CFG}_network \
--DNANO_GUI=ON \
--DPORTABLE=1 \
--DCMAKE_BUILD_TYPE=${CONFIGURATION} \
--DCMAKE_VERBOSE_MAKEFILE=ON \
--DQt5_DIR=${qt_dir} \
--DCI_BUILD=true \
--DCI_VERSION_PRE_RELEASE="${ci_version_pre_release}" \
-..
+$(dirname "$BASH_SOURCE")/build.sh package
 
-if [[ "$OS" == 'Linux' ]]; then
-    cmake --build ${PWD} --target package --config ${CONFIGURATION} -- -j$(nproc)
-else
-    sudo cmake --build ${PWD} --target package --config ${CONFIGURATION} -- -j2
-fi
-
-popd
